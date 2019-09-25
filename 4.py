221512d9f10f272e77db69b3a1743a9d6c3444f2
Snickers97 <jacobhandley50@gmail.com>
2019-09-25 00:39:18 -0400

    Added fta

--- 4.py
+++ 4.py
@@ -1,5 +1,6 @@
 import scipy.io as sci
 import matplotlib.pyplot as plt
+import statistics as stats
 clocks = sci.loadmat('clocktime')
 drift_rates = sci.loadmat('rho_solution')
 rho = drift_rates['rho'][0]
@@ -21,3 +22,23 @@ for x in rho:
 rint = 0.5/sum
 rint = round(rint,3)
 print('Resynchronization interval:',rint,'seconds')
+
+def FTA(t):
+    y = []
+    size = len(t)
+    for i in range(1,size): #don't do this for tref, so starting at 1
+        x = []
+        for j in range(0,size):
+            a = t[j]-t[i]
+            x.append(a)
+        x.sort()
+        #For now, just assuming that k == 1
+        x.pop()
+        x.pop(0)
+        a = stats.mean(x)
+        y.append(t[i]+a)
+    return y
+
+x = [4.06,4.04,4.07,4.08,4.18,100,1000]
+y = FTA(x)
+print('Final:',y)
