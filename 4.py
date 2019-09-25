15cb58378579bc21d71490e5e696115139d62c7d
Snickers97 <jacobhandley50@gmail.com>
2019-09-25 10:25:50 -0400

    Finished and commented

--- 4.py
+++ 4.py
@@ -20,9 +20,10 @@ for x in rho:
     sum += abs(x)
 
 rint = 0.5/sum
-rint = round(rint,3)
-print('Resynchronization interval:',rint,'seconds')
+print('Resynchronization interval:',round(rint,3),'seconds')
 
+#Calculates the difference between each clock and the average value,
+#then returns an array of the differences to be applied to the clock times
 def FTA(t):
     y = []
     size = len(t)
@@ -36,9 +37,25 @@ def FTA(t):
         x.pop()
         x.pop(0)
         a = stats.mean(x)
-        y.append(t[i]+a)
+        y.append(a)
     return y
 
-x = [4.06,4.04,4.07,4.08,4.18,100,1000]
-y = FTA(x)
-print('Final:',y)
+interval = t[0][0]
+for i in range(0,len(t[0])):
+    if t[0][i] >= interval: #run this code at each resync interval
+        x = []
+        for j in range(0,len(t)):   #Create a 1D array of the clock values at rsync time
+            x.append(t[j][i])
+        y = FTA(x)  #Calculate differnce between each clock and true value, store in array y
+        for j in range(1,len(t)):
+            for k in range(i,len(t[0])):
+                t[j][k] += y[j-1]   #Add y value for each clock to all remaining values in the big array
+        interval = t[0][i]+rint
+
+for i in range(0,len(t)):
+    plt.plot(t[0],t[i],label='clock'+str(i))
+plt.title('Internal Synchronization')
+plt.xlabel('Reference clock (clock 0)')
+plt.ylabel('Local clock')
+plt.legend(loc='upper left')
+plt.show()
